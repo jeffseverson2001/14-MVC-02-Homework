537d16df4e33e16601b79c10ab84c3c546562a0f
jeffseverson2001 <jeff@zealouscreations.com>
2021-08-13 22:45:50 -0500

    Last Friday Update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,37 +1,40 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    /*
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    try {
+        // Get all projects and JOIN with user data
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+            order: [
+                ['date_created', 'DESC'],
+            ],
+        });
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+        // Serialize data so the template can read it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-    */
-  } catch (err) {
-    res.status(500).json(err);
-  }
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
+/*
 router.get('/blog/:id', async (req, res) => {
   try {
-    /*
     const projectData = await Project.findByPk(req.params.id, {
       include: [
         {
@@ -47,21 +50,29 @@ router.get('/blog/:id', async (req, res) => {
       ...project,
       logged_in: req.session.logged_in
     });
-    */
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+*/
+
+router.get('/signup', (req, res) => {
+    //if (req.session.logged_in) {
+    //    res.redirect('/homepage');
+    //    return;
+    //}
+    res.render('signup');
+});
+
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/homepage');
-    return;
-  }
-
-  res.render('login');
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
