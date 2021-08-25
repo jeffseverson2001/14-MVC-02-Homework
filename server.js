b61efbd180adb22df9961fee250c8ab807dd4874
jeffseverson2001 <jeff@jefflizseverson.com>
2021-08-25 17:53:22 -0500

    Update For Heroku

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  //secret: 'Super-secret-secret',
-  secret: process.env.SECRET,
+  secret: 'Super-secret-secret',
+  //secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
